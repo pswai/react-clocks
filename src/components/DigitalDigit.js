61a357865751106399e3b355e79a20561b0db9e3
PSWai <parksoon_my@hotmail.com>
2018-03-17 14:37:28 +0800

    Consolidate notch info

--- src/components/DigitalDigit.js
+++ src/components/DigitalDigit.js
@@ -63,75 +63,61 @@ const DigitalDigit = props => {
   const notches = {
     a: {
       isOn: [0, 2, 3, 5, 6, 7, 8, 9].includes(parsedDigit),
+      x: spacing / 2,
+      width: horizontalNotchWidth,
+      height: thickness,
     },
     b: {
       isOn: [0, 1, 2, 3, 4, 7, 8, 9].includes(parsedDigit),
+      x: width - thickness,
+      y: spacing / 2,
+      width: thickness,
+      height: verticalNotchHeight,
     },
     c: {
       isOn: [0, 1, 3, 4, 5, 6, 7, 8, 9].includes(parsedDigit),
+      x: width - thickness,
+      y: height / 2 - thickness + spacing / 2,
+      width: thickness,
+      height: verticalNotchHeight,
     },
     d: {
       isOn: [0, 2, 3, 5, 6, 8, 9].includes(parsedDigit),
+      x: spacing / 2,
+      y: height - 2 * thickness,
+      width: horizontalNotchWidth,
+      height: thickness,
     },
     e: {
       isOn: [0, 2, 6, 8].includes(parsedDigit),
+      y: height / 2 - thickness + spacing / 2,
+      width: thickness,
+      height: verticalNotchHeight,
     },
     f: {
       isOn: [0, 4, 5, 6, 8, 9].includes(parsedDigit),
+      y: spacing / 2,
+      width: thickness,
+      height: verticalNotchHeight,
     },
     g: {
-      isOn: [0, 2, 3, 4, 5, 6, 8, 9].includes(parsedDigit),
+      isOn: [2, 3, 4, 5, 6, 8, 9].includes(parsedDigit),
+      x: spacing / 2,
+      y: height / 2 - thickness,
+      width: horizontalNotchWidth,
+      height: thickness,
     },
   };
 
   return (
     <svg {...others} height={height} width={width}>
-      <Notch
-        {...notches.a}
-        x={spacing / 2}
-        width={horizontalNotchWidth}
-        height={thickness}
-      />
-      <VerticalNotch
-        {...notches.b}
-        x={width - thickness}
-        y={spacing / 2}
-        width={thickness}
-        height={verticalNotchHeight}
-      />
-      <VerticalNotch
-        {...notches.c}
-        x={width - thickness}
-        y={height / 2 - thickness + spacing / 2}
-        width={thickness}
-        height={verticalNotchHeight}
-      />
-      <Notch
-        {...notches.d}
-        x={spacing / 2}
-        y={height - 2 * thickness}
-        width={horizontalNotchWidth}
-        height={thickness}
-      />
-      <VerticalNotch
-        {...notches.e}
-        y={height / 2 - thickness + spacing / 2}
-        width={thickness}
-        height={verticalNotchHeight}
-      />
-      <VerticalNotch
-        {...notches.f}
-        y={spacing / 2}
-        width={thickness}
-        height={verticalNotchHeight}
-      />
-      <Notch
-        {...notches.g}
-        x={spacing / 2}
-        y={height / 2 - thickness}
-        width={horizontalNotchWidth}
-        height={thickness}
-      />
+      <Notch {...notches.a} />
+      <VerticalNotch {...notches.b} />
+      <VerticalNotch {...notches.c} />
+      <Notch {...notches.d} />
+      <VerticalNotch {...notches.e} />
+      <VerticalNotch {...notches.f} />
+      <Notch {...notches.g} />
     </svg>
   );
 };
@@ -141,7 +127,7 @@ DigitalDigit.prototypes = {
   digit: PropTypes.oneOf([
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-  ]).isRequired,
+  ]),
   width: PropTypes.number,
   height: PropTypes.number,
   spacing: PropTypes.number,
@@ -149,6 +135,7 @@ DigitalDigit.prototypes = {
 };
 
 DigitalDigit.defaultProps = {
+  digit: null,
   width: 50,
   height: 100,
   spacing: 2,
--- src/stories/DigitalDigit.js
+++ src/stories/DigitalDigit.js
@@ -6,5 +6,14 @@ import { setTimeout } from "timers";
 
 storiesOf("DigitalDigit", module)
   .add("empty", () => <DigitalDigit />)
-  .add("with number", () => <DigitalDigit digit={3} />)
+  .add("0", () => <DigitalDigit digit={0} />)
+  .add("1", () => <DigitalDigit digit={1} />)
+  .add("2", () => <DigitalDigit digit={2} />)
+  .add("3", () => <DigitalDigit digit={3} />)
+  .add("4", () => <DigitalDigit digit={4} />)
+  .add("5", () => <DigitalDigit digit={5} />)
+  .add("6", () => <DigitalDigit digit={6} />)
+  .add("7", () => <DigitalDigit digit={7} />)
+  .add("8", () => <DigitalDigit digit={8} />)
+  .add("9", () => <DigitalDigit digit={9} />)
   .add("with string", () => <DigitalDigit digit="5" />);
