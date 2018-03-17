136c917480ea92319e53c352c3cbe0dff76cf292
PSWai <parksoon_my@hotmail.com>
2018-03-17 14:01:40 +0800

    Added digit info

--- src/components/DigitalDigit.js
+++ src/components/DigitalDigit.js
@@ -2,11 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Notch = props => {
-  const { width, height } = props;
+  const { width, height, isOn } = props;
   const style = {
-    fill: "lime",
-    stroke: "black",
-    strokeWidth: 1,
+    fill: isOn ? "black" : "e6e6e6",
   };
   const vMid = height / 2;
   const points = [
@@ -26,11 +24,9 @@ const Notch = props => {
 };
 
 const VerticalNotch = props => {
-  const { width, height } = props;
+  const { width, height, isOn } = props;
   const style = {
-    fill: "lime",
-    stroke: "black",
-    strokeWidth: 1,
+    fill: isOn ? "black" : "e6e6e6",
   };
   const hMid = width / 2;
   const points = [
@@ -49,18 +45,93 @@ const VerticalNotch = props => {
   );
 };
 
+/*
+ * Notch Label
+ * 
+ *    A
+ * F     B
+ *    G
+ * E     C
+ *    D
+ */
 const DigitalDigit = props => {
-  const digit = parseInt(props.digit, 10);
+  const { digit, width, height, spacing, thickness, ...others } = props;
+  const horizontalNotchWidth = width - spacing;
+  const verticalNotchHeight = height / 2 - spacing;
+
+  const parsedDigit = parseInt(digit);
+  const notches = {
+    a: {
+      isOn: [0, 2, 3, 5, 6, 7, 8, 9].includes(parsedDigit),
+    },
+    b: {
+      isOn: [0, 1, 2, 3, 4, 7, 8, 9].includes(parsedDigit),
+    },
+    c: {
+      isOn: [0, 1, 3, 4, 5, 6, 7, 8, 9].includes(parsedDigit),
+    },
+    d: {
+      isOn: [0, 2, 3, 5, 6, 8, 9].includes(parsedDigit),
+    },
+    e: {
+      isOn: [0, 2, 6, 8].includes(parsedDigit),
+    },
+    f: {
+      isOn: [0, 4, 5, 6, 8, 9].includes(parsedDigit),
+    },
+    g: {
+      isOn: [0, 2, 3, 4, 5, 6, 8, 9].includes(parsedDigit),
+    },
+  };
 
   return (
-    <svg height="400" width="200">
-      <Notch width="200" height="50" />
-      <VerticalNotch x="150" width="50" height="200" />
-      <VerticalNotch x="150" y="150" width="50" height="200" />
-      <Notch y="300" width="200" height="50" />
-      <VerticalNotch y="150" width="50" height="200" />
-      <VerticalNotch width="50" height="200" />
-      <Notch y="150" width="200" height="50" />
+    <svg {...others} height={height} width={width}>
+      <Notch
+        {...notches.a}
+        x={spacing / 2}
+        width={horizontalNotchWidth}
+        height={thickness}
+      />
+      <VerticalNotch
+        {...notches.b}
+        x={width - thickness}
+        y={spacing / 2}
+        width={thickness}
+        height={verticalNotchHeight}
+      />
+      <VerticalNotch
+        {...notches.c}
+        x={width - thickness}
+        y={height / 2 - thickness + spacing / 2}
+        width={thickness}
+        height={verticalNotchHeight}
+      />
+      <Notch
+        {...notches.d}
+        x={spacing / 2}
+        y={height - 2 * thickness}
+        width={horizontalNotchWidth}
+        height={thickness}
+      />
+      <VerticalNotch
+        {...notches.e}
+        y={height / 2 - thickness + spacing / 2}
+        width={thickness}
+        height={verticalNotchHeight}
+      />
+      <VerticalNotch
+        {...notches.f}
+        y={spacing / 2}
+        width={thickness}
+        height={verticalNotchHeight}
+      />
+      <Notch
+        {...notches.g}
+        x={spacing / 2}
+        y={height / 2 - thickness}
+        width={horizontalNotchWidth}
+        height={thickness}
+      />
     </svg>
   );
 };
@@ -69,8 +140,19 @@ DigitalDigit.prototypes = {
   // prettier-ignore
   digit: PropTypes.oneOf([
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-  ])
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  ]).isRequired,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  spacing: PropTypes.number,
+  thickness: PropTypes.number,
+};
+
+DigitalDigit.defaultProps = {
+  width: 50,
+  height: 100,
+  spacing: 2,
+  thickness: 10,
 };
 
 export default DigitalDigit;
